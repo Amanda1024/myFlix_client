16b85fe3b4a36674cde2d8ce26709b021290eb9d
Amanda1024 <88503704+Amanda1024@users.noreply.github.com>
2021-10-02 14:10:21 -0700

    swapped out double quotes with single, for consistency

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,7 +5,7 @@ export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;  
     return (
-    <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
+    <div className='movie-card' onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
     );
   }
 }
