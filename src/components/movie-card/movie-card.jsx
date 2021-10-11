2aa09f52326373627f3fd714b2212715f3456e69
Amanda1024 <88503704+Amanda1024@users.noreply.github.com>
2021-10-11 10:38:32 -0700

    added Link via Boorstrap, switched to Bootstrap Button

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from 'react-router-dom';
+
 import './movie-card.scss';
 
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
     return (
       <div className='card'>
       <Card>
@@ -16,7 +18,9 @@ export class MovieCard extends React.Component {
         <Card.Body>
           <Card.Title>{movie.Title}</Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <button className='button' onClick={() => onMovieClick(movie)} variant='link'>Open</button>
+          <Link to={`/movies/${movie._id}`}>
+            <Button className='button'variant='link'>Open</Button>
+          </Link>
         </Card.Body>
       </Card>
       </div>
