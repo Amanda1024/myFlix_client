db6197b4cc52c47c5da9f1f28bd8939d528ee44c
Amanda1024 <88503704+Amanda1024@users.noreply.github.com>
2021-10-13 13:40:16 -0700

    imported scss file, added className for styling access

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
+import './navbar-view.scss';
 
 export class NavBar extends React.Component {
     constructor() {
@@ -25,16 +26,17 @@ export class NavBar extends React.Component {
         if (!user) return null;
 
         return (
-            <Navbar bg='light' collapseOnSelect fixes='top' expand='lg' variant='light' >
+            <Navbar collapseOnSelect fixes='top' expand='lg' variant='light' className='navbar'>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id='responsive-navbar-nav'>
+                    <h3 className='myflix'>myFlix</h3>
                     <Nav.Link as={Link} to={movies} className='link-text'>
                         Movies
                     </Nav.Link>
                     <Nav.Link as={Link} to={profile} className='link-text'>
                         Profile
                     </Nav.Link>
-                    <Nav.Link to={'/'} onClick={this.onLoggedOut}>
+                    <Nav.Link to={'/'} onClick={this.onLoggedOut} className='logout'>
                         Log Out
                     </Nav.Link>
                 </Navbar.Collapse>
